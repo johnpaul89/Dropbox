1275254f05ce53d98b8d9ccdc6ee300707efab36
JohnPaul Gitata <johnpaulgitata@gmail.com>
2018-05-24 06:17:47 +0300

    Linked Log in with the name Dropdown and email

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,36 @@
+//business logic
+function Log(first, last) {
+  this.firstName = first;
+  this.lastName = last;
+}
+
 $(document).ready(function(){
   setTimeout(popup, 2000);
   function popup() {
-    $("#logindiv").css("display", "block");
+  $("#logindiv").css("display", "block");
   }
   $("#login #cancel").click(function(){
-    $(this).parent().parent().hide();
+  $(this).parent().parent().hide();
   });
-});
+  $("#login #loginbtn").click(function(){
+  $(this).parent().parent().hide();
+  });
 
-$("#loginbtn").click(function(){
-  var name = $("#name").val();
-  var email = $("#email").vaal();
+
+$("#loginbtn").click(function(event){
+  event.preventDefault();
+  var inputtedName = $("input#name").val();
+  var inputtedEmail = $("#email").val();
+  var newLog = new Log(inputtedName, inputtedEmail);
+
+  $("h4#log").append("<h4><span class='login'>" + newLog.firstName + "</span></h4>")
+
+  $(".login").last().click(function(){
+    $("#show-login").show();
+
+    $(".last-name").text(newLog.lastName);
+  });
+  $("input#new-first-name").val("");
+  $("input#new-last-name").val("");
+});
 });
